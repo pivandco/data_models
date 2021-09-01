90bd3045aa19cbc03a63f27d7357b5df7280d1cc
javabird25 <shockck84@gmail.com>
2021-09-01 21:28:52 +0500

     Shi(p|t)

---
+++ ship/csharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace csharp
+{
+    internal enum Direction
+    {
+        North,
+        East,
+        South,
+        West,
+    }
+
+    internal enum Command
+    {
+        Back = -2,
+        Left,
+        Right = 1,
+    }
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var oldDirection =
+                Input<Direction>("Введите текущее направление корабля: ");
+            var command = Input<Command>("Введите команду: ");
+            var newDirection = ChangeDirection(oldDirection, command);
+            Console.WriteLine($"Новое направление: {newDirection}");
+        }
+
+        private static T Input<T>(string message) where T : struct
+        {
+            Console.Write(message);
+            var success = false;
+            while (!success)
+            {
+                success = Enum.TryParse(Console.ReadLine(), true, out T value);
+                if (success) return value;
+            }
+
+            throw new InvalidOperationException("Unreachable");
+        }
+
+        private static Direction ChangeDirection(Direction oldDirection, Command command)
+        {
+            return (Direction) (((int) oldDirection + (int) command + 4) % 4);
+        }
+    }
+}
