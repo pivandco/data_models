ce6a138966d3c034e5f7d2a69e13043f7abd76ef
Endoer <89925327+Endoer@users.noreply.github.com>
2021-09-02 21:56:51 +0500

    Update data_models1(js).js

--- data_models1(js).js
+++ data_models1(js).js
@@ -1,6 +1,6 @@
 "use strict";
 let curent_direction = "North";
-let repeat;
+
 var Direction;
 (function (Direction) {
     Direction[Direction["North"] = 0] = "North";
@@ -8,21 +8,24 @@ var Direction;
     Direction[Direction["South"] = 2] = "South";
     Direction[Direction["West"] = 3] = "West";
 })(Direction || (Direction = {}));
+
 var Commands = {
     Right : 1,
     Back : 2,
     Left : 3
 };
+
 let set_Commands = new Set();
 for (let key in Commands){
 set_Commands.add(key);
 }
+
+let repeat;
 do {
     do {
         var command = prompt("Command?(Right,Left,Back)", "");
     } while (!(set_Commands.has(command)));
     curent_direction = Direction[(Direction[curent_direction] + Commands[command]) % 4];
-    ;
     alert('Direction on ' + curent_direction);
-    repeat = confirm("Repeat?");
+    repeat = confirm("Change direction?");
 } while (repeat);
