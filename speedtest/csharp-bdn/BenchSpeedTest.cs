837942551282330a1438aa28bed2cff871e20cd6
Vladimir <33591723+jeffors@users.noreply.github.com>
2021-09-14 19:21:38 +0500

    BenchmarkDotNet (C#)

---
+++ speedtest/csharp-bdn/BenchSpeedTest.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace SPEEDTEST 
+{
+    public class SearchElement
+    {
+        [Benchmark]
+        public int LinearSearch()
+        {
+            int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int x = 5, i = 0;
+
+            while (i < a.Length - 1 && a[i] != x)
+            {
+                i++;
+            }
+
+            return a[i] == x ? i : -1;
+        }
+        [Benchmark]
+        public int BarrierSearch()
+        {
+            int x = 5, i = 0;
+            int[] a = new int[11] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, x };
+
+            while (a[i] != x)
+            {
+                i++;
+            }
+
+            return i != a.Length -1 ? i : -1;
+        }
+        [Benchmark]
+        public int BinarySearch()
+        {
+            int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int x = 5;
+            int i = 0, j = a.Length;
+
+            while (i <= j)
+            {
+                int k = (i + j) / 2;
+
+                if (x == a[k]) return k;
+                else if (x < a[k]) j = k - 1;
+                else i = i + 1;
+            }
+
+            return -1;
+        }
+    }
+
+    public class BenchSpeedTest
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<SearchElement>();
+        }
+    }
+}
---
+++ speedtest/csharp-bdn/README.md
@@ -0,0 +1,25 @@
+# BenchmarkDotNet 
+Лучший пакет для проверки быстродействия вашего кода
+
+``` ini
+
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.22000
+AMD Ryzen 5 3600, 1 CPU, 12 logical and 6 physical cores
+.NET SDK=6.0.100-preview.7.21379.14
+  [Host]     : .NET 6.0.0 (6.0.21.37719), X64 RyuJIT
+  DefaultJob : .NET 6.0.0 (6.0.21.37719), X64 RyuJIT
+
+
+```
+|        Method |     Mean |     Error |    StdDev |   Median |
+|-------------- |---------:|----------:|----------:|---------:|
+|  LinearSearch | 7.436 ns | 0.1355 ns | 0.1267 ns | 7.468 ns |
+| BarrierSearch | 7.474 ns | 0.1764 ns | 0.2473 ns | 7.324 ns |
+|  BinarySearch | 5.704 ns | 0.1506 ns | 0.4442 ns | 5.847 ns |
+
+## Legends 
+  Mean   : Arithmetic mean of all measurements
+  Error  : Half of 99.9% confidence interval
+  StdDev : Standard deviation of all measurements
+  Median : Value separating the higher half of all measurements (50th percentile)
+  1 ns   : 1 Nanosecond (0.000000001 sec)
