c6c082e7c85fc25495b6ec8ab53086e32ff87916
javabird25 <shockck84@gmail.com>
2021-09-30 10:33:18 +0500

    Ctrl+Alt+L

--- brackets/js/script.js
+++ brackets/js/script.js
@@ -1,26 +1,26 @@
 let baseString = prompt("Enter string", "");
 let upperString = [];
-let bracket = ['[', ']', '(', ')', '{', '}'];
-let rightBrackets=['()','[]','{}']
+let bracket = ["[", "]", "(", ")", "{", "}"];
+let rightBrackets = ["()", "[]", "{}"];
 for (let i = 0; i < baseString.length; i++) {
-    if (bracket.indexOf(baseString[i]) !== -1) {
-        upperString.push(baseString[i]);
-    }
+  if (bracket.indexOf(baseString[i]) !== -1) {
+    upperString.push(baseString[i]);
+  }
 }
 let next = true;
-upperString=upperString.join('');
+upperString = upperString.join("");
 while (next) {
-    next = false;
-    rightBrackets.forEach(key =>{
-        if (upperString.indexOf(key) !== -1) {
-            upperString=upperString.replaceAll(key, '')
-            next = true;
-            console.log(upperString);
-        }
-
-    })
+  next = false;
+  rightBrackets.forEach((key) => {
+    if (upperString.indexOf(key) !== -1) {
+      upperString = upperString.replaceAll(key, "");
+      next = true;
+      console.log(upperString);
+    }
+  });
 }
-if (upperString.length===0){
-    alert("Скобки корректны")
+if (upperString.length === 0) {
+  alert("Скобки корректны");
+} else {
+  alert("Скобки не корректны");
 }
-else alert("Скобки не корректны")
